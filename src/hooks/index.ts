bf291dc4f5763b0728ba004675c08c0282fb7a64
starbor <cfb4356@163.com>
2021-12-30 12:25:08 -0500

    fix: update the readme

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,4 @@
 import { useState } from 'react'
-import { request } from '../utils/index'
-import { Message } from '@alifd/next'
-
-interface ParamsProps {
-  keyword?: string;
-  url?: string;
-}
 
 const useIndex = () => {
   const [activeKey, setActiveKey] = useState('amazon')
